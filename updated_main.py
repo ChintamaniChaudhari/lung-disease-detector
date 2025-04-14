ba88242dd8e1cf17e87ce64625918d36de455a46
ChintamaniChaudhari <chintamanichaudhari123@gmail.com>
2025-04-14 12:02:50 +0530

    Update updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 from updated_util import classify, set_background
 
 set_background("BackGround.jpg")
-st.info("Trying to load background image...")
 
 def show_output_chart(predictions, class_names):
     colors = ['#fddccf', '#fce4ec', '#8e44ad', '#d6eaf8', '#fddccf']  # customize as needed
